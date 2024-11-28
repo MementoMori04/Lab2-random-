52507be978b7db8ef9e7d91f0c1edc898ea7b966
MementoMori04 <144354338+MementoMori04@users.noreply.github.com>
2024-11-29 01:48:03 +0900

    Рандомайзер

--- Lab2(random)/Form1.cs
+++ Lab2(random)/Form1.cs
@@ -16,5 +16,33 @@ namespace Lab2_random_
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int min = int.Parse(textBox1.Text);
+                int max = int.Parse(textBox2.Text);
+
+                if (min >= max)
+                {
+                    MessageBox.Show("Минимальное значение должно быть меньше максимального.");
+                    return;
+                }
+
+                Random random = new Random();
+                int randomNumber = random.Next(min, max + 1); // +1, чтобы включить max в диапазон
+
+                label1.Text = randomNumber.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите целые числа в поля ввода.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+        }
     }
 }
